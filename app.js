362bb23a811ee19512dbdb0615ac2d69574a05d6
Md Abdul Hakim <mail@hakim.one>
2021-03-02 15:53:55 +0600

    Fixed slider display image issue.

--- app.js
+++ app.js
@@ -38,13 +38,15 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  console.log(element)
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
+  console.log(item)
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    sliders.pop(img);
   }
 }
 var timer
